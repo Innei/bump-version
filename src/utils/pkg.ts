dca0efb1de476364612e46076bdd000fe24216c4
Innei <tukon479@gmail.com>
2022-09-22 20:55:29 +0800

    fix: entry
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/utils/pkg.ts
+++ src/utils/pkg.ts
@@ -1,25 +1,8 @@
 // @ts-check
 import { join } from 'path'
-import { argv } from 'zx'
 
 import { readFileSync } from 'fs'
 import semver from 'semver'
-
-if (
-  argv?.['version'] ||
-  argv?.['v'] ||
-  ['v', 'version'].includes(argv?.['_'][0])
-) {
-  const json = JSON.parse(
-    readFileSync(new URL('../package.json', import.meta.url), {
-      encoding: 'utf-8',
-    }),
-  )
-
-  console.log(json.version)
-
-  process.exit(0)
-}
 
 export function getPackageJson() {
   const PKG_PATH = join(String(process.cwd()), '/package.json')
