d78f12e8ea31dc51277e4553dcd3b38ff0059174
Innei <tukon479@gmail.com>
2022-09-28 22:49:19 +0800

    feat: detect package manger
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/utils/memo.ts
+++ src/utils/memo.ts
@@ -9,3 +9,15 @@ export const memoReturnValueFunction = <T extends (...args: any[]) => any>(
     return cache
   }) as T
 }
+
+export const memoReturnValueAsyncFunction = <T extends (...args: any[]) => any>(
+  fn: T,
+): T => {
+  let cache: any = undefined
+  return (async (...args: any[]) => {
+    if (cache === undefined) {
+      cache = await fn(...args)
+    }
+    return cache
+  }) as T
+}
