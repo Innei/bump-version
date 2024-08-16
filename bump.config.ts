bebec4f1be0b8a79978f1cc020882463bdf4ce78
Innei <i@innei.in>
2024-08-16 15:23:28 +0800

    fix: config
    Signed-off-by: Innei <i@innei.in>

--- bump.config.ts
+++ bump.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from './src/index.mjs'
 
 export default defineConfig({
   leading: ['git pull --rebase', 'pnpm i', 'npm run build'],
-  trailing: [
-    "gh pr create --title 'chore: Release v${NEW_VERSION}' --body 'v${NEW_VERSION}' --base main --head dev",
-  ],
+  trailing: [],
   publish: true,
   changelog: false,
   mode: 'independent',
