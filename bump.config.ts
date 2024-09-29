1b390e2d4885e341c8cd5eb0648c7e1b3e9f12b3
Innei <i@innei.in>
2024-09-29 22:35:10 +0800

    chore: build after update version
    Signed-off-by: Innei <i@innei.in>

--- bump.config.ts
+++ bump.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from './src/index.mjs'
 
 export default defineConfig({
-  leading: ['git pull --rebase', 'pnpm i', 'npm run build'],
-  trailing: [],
+  leading: ['git pull --rebase', 'pnpm i'],
+  trailing: ['npm run build'],
   publish: true,
   changelog: false,
   mode: 'independent',
